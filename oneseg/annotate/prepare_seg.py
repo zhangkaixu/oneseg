f286aceda74611566121a1141b2efd23fa556fdb
zhangkaixu <zhangkaixu@hotmail.com>
2014-04-01 08:33:12 +0800

    增量标注测试完成，但标注规范还……

--- oneseg/annotate/prepare_seg.py
+++ oneseg/annotate/prepare_seg.py
@@ -3,12 +3,18 @@
 import sys
 import json
 
+def seq_to_anno(y):
+    anno = []
+    for word in y :
+        anno+=(['-']*(len(word)-1)+['|'])
+    return ''.join(anno[:-1])
+
 if __name__ == '__main__':
     for line in sys.stdin :
         line = line.split()
+        predicted = seq_to_anno(line)
         line = ''.join(line)
         anno = ['?' for i in range(len(line)-1)]
-        record = {'raw':line,'anno':anno}
+        record = {'raw':line,'anno':anno, 'predicted' : predicted}
         print(json.dumps(record, ensure_ascii = False))
-
 
